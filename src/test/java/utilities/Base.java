d39f1c79c6549c9eecbf419b0f8572217c2cd607
emrahsacan <you@example.com>
2023-05-09 20:19:15 +0300

    Initial commit

---
+++ src/test/java/utilities/Base.java
@@ -0,0 +1,20 @@
+package utilities;
+
+import org.openqa.selenium.WebDriver;
+
+public class Base {
+    public WebDriver setDriver(WebDriver driver, String browser){
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
