bd7155429d60211d4f6abb413e95beeac62f0e39
emrahsacan <you@example.com>
2023-05-10 07:39:22 +0300

    emrah 06.05.2023

--- src/test/java/stepdefinitions/AmazonStepdefinitions.java
+++ src/test/java/stepdefinitions/AmazonStepdefinitions.java
@@ -15,28 +15,28 @@ import java.util.List;
 
 public class AmazonStepdefinitions {
 
-    AmazonPage amazonPage= new AmazonPage();
+   AmazonPage amazonPage= new AmazonPage();
 
-    @Given("kullanici amazon anasayfaya gider")
-    public void kullanici_amazon_anasayfaya_gider() {
-        Driver.getDriver().get(ConfigReader.getProperty("amazonUrl"));
-    }
-    @Then("amazon arama kutusuna Nutella yazip aratir")
-    public void amazon_arama_kutusuna_nutella_yazip_aratir() {
-        amazonPage= new AmazonPage();
-        amazonPage.amazonAramaKutusu.sendKeys("Nutella" + Keys.ENTER);
-    }
-    @Then("arama sonuclarinin Nutella icerdigini test eder")
-    public void arama_sonuclarinin_nutella_icerdigini_test_eder() {
-        String actualAramaSonucu= amazonPage.aramaSonucuElementi.getText();
-        String expectedKelime="Nutella";
+  //  @Given("kullanici amazon anasayfaya gider")
+  //  public void kullanici_amazon_anasayfaya_gider() {
+  //      Driver.getDriver().get(ConfigReader.getProperty("amazonUrl"));
+  //  }
+  //  @Then("amazon arama kutusuna Nutella yazip aratir")
+  //  public void amazon_arama_kutusuna_nutella_yazip_aratir() {
+  //      amazonPage= new AmazonPage();
+  //      amazonPage.amazonAramaKutusu.sendKeys("Nutella" + Keys.ENTER);
+  //  }
+  //  @Then("arama sonuclarinin Nutella icerdigini test eder")
+  //  public void arama_sonuclarinin_nutella_icerdigini_test_eder() {
+  //      String actualAramaSonucu= amazonPage.aramaSonucuElementi.getText();
+  //      String expectedKelime="Nutella";
 
-        Assert.assertTrue(actualAramaSonucu.contains(expectedKelime));
-    }
-    @Then("sayfayi kapatir")
-    public void sayfayi_kapatir() {
-        Driver.closeDriver();
-    }
+  //      Assert.assertTrue(actualAramaSonucu.contains(expectedKelime));
+  //  }
+  //  @Then("sayfayi kapatir")
+  //  public void sayfayi_kapatir() {
+  //      Driver.closeDriver();
+  //  }
 
     @Then("amazon arama kutusuna Java yazip aratir")
     public void amazonAramaKutusunaJavaYazipAratir() {
--- src/test/java/utilities/Base.java
+++ src/test/java/utilities/Base.java
@@ -1,20 +1,59 @@
 package utilities;
 
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 public class Base {
+
+    DesiredCapabilities cap= new DesiredCapabilities();  // Bir Capabilites oluşturmak gerekiyordu
+
+    static String hubURL="http://192.168.0.102:4444";  // remote bilgisayarın id'si -->192.168.0.102  root'u -->4444
     public WebDriver setDriver(WebDriver driver, String browser){
 
+        if (browser=="chrome") {
+
+            cap.setBrowserName("chrome");
+            cap.setVersion("113.0.5672.93");
+            cap.setPlatform(Platform.ANY);
+
+            ChromeOptions optionsChrome = new ChromeOptions();
+            optionsChrome.merge(cap);  // merge methodunu kullanarak cap objemizi yukarıdaki bilgileri birleştirerek
+                                     // bir options haline getirdik
+
+        }
+            if (browser=="edge"){
+
+                cap.setBrowserName("edge");
+                cap.setVersion("113.0.1774.35");
+                cap.setPlatform(Platform.ANY);
+
+                EdgeOptions optionsEdge = new EdgeOptions();
+                optionsEdge.merge(cap);
+
+
+            }
+
+
+            try {
+                driver = new RemoteWebDriver(new URL(hubURL),cap);
+            } catch (MalformedURLException e) {
+                throw new RuntimeException(e);
+            }
+
+            driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+            driver.manage().window().maximize();
+
+        return  driver;
+
     }
-
-
-
-
-
-
-
-
-
 
 
 }
